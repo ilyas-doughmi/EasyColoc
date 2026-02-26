d397bc0573608e1eac71d7750ee01d3352689e97
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-02-26 12:57:54 +0000

    feat: add invitation email template for colocation invitations

---
+++ web/resources/views/emails/invitation.blade.php
@@ -0,0 +1,8 @@
+Bonjour ! 👋 <br>
+
+Vous avez été invité(e) à rejoindre la colocation "{{ $colocationName }}" sur EasyColoc. <br>
+
+Cliquez sur ce lien pour accepter l'invitation : <br>
+{{ url('/invitation?token=' . $invitation->token) }} <br>
+
+L'équipe EasyColoc
