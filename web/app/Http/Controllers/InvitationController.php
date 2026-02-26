9715bba79bf3273f0838d8f3fab3a14fa9c7acde
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-02-26 11:26:04 +0000

    feat: implement store method for sending invitations

--- web/app/Http/Controllers/InvitationController.php
+++ web/app/Http/Controllers/InvitationController.php
@@ -3,15 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\InvitationStoreRequest;
+use App\Models\Invitation;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\InvitationMail;
 
 class InvitationController extends Controller
 {
-    public function codeGenerator()
+    public function store(InvitationStoreRequest $request)
     {
-        
-    }
-    public function sendInvitation(Request $request)
-    {
-        
+        $invitation = Invitation::create([
+            'email' => $request->email,
+            'colocation_id' => $request->colocation_id,
+            'token' => Str::random(20),
+        ]);
+
+        Mail::to($request->email)->send(new InvitationMail($invitation));
+
+        return redirect()->back()->with('success', 'Invitation envoyée !');
     }
 }
