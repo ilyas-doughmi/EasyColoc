bd2a720d3ae7d4fd3fc794ceb64bdef822ea39b0
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-02-26 10:20:42 +0000

    feat: add fillable properties and relationship method in Invitation model

--- web/app/Models/Invitation.php
+++ web/app/Models/Invitation.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Invitation extends Model
 {
-    //
+    protected $fillable = ['token','colocation_id','email','is_active'];
+    
+    public function colocations()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
 }
