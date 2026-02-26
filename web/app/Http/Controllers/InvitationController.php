cd6d1d8e8a93ac5c191d6bfc27a8fb736375379a
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-02-26 12:56:41 +0000

    feat: refactor sendInvitation method to use active colocation and improve token generation

--- web/app/Http/Controllers/InvitationController.php
+++ web/app/Http/Controllers/InvitationController.php
@@ -6,21 +6,48 @@ use Illuminate\Http\Request;
 use App\Http\Requests\InvitationStoreRequest;
 use App\Models\Invitation;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
-use App\Mail\InvitationMail;
+use App\Mail\ColocationInvitation;
 
 class InvitationController extends Controller
 {
-    public function store(InvitationStoreRequest $request)
-    {
+
+   public function codeGenerator()
+   {
+        $token = Str::random(32);
+        return $token;
+   }
+
+   public function sendInvitation(Request $request)
+   {
+        $request->validate([
+            'email' => 'required|email',
+            'colocation_id' => 'required|exists:colocations,id',
+        ]);
+        
+        $user = Auth::user();
+
+        $activeColocation = $user->colocations()->wherePivot('status', 'active')->withPivot('role')->first();
+
+        if (!$activeColocation) {
+            return redirect()->back()->with('error', 'Vous devez avoir une colocation active pour envoyer des invitations.');
+        }
+
+        if($activeColocation->pivot->role != 'owner'){
+            return redirect()->back()->with('error', 'Vous devez être owner pour envoyer des invitations.');
+        }
+
+        $token = $this->codeGenerator();
+
         $invitation = Invitation::create([
             'email' => $request->email,
-            'colocation_id' => $request->colocation_id,
-            'token' => Str::random(20),
+            'colocation_id' => $activeColocation->id,
+            'token' => $token,
         ]);
 
-        Mail::to($request->email)->send(new InvitationMail($invitation));
+        Mail::to($request->email)->send(new ColocationInvitation($invitation, $activeColocation->name));
 
         return redirect()->back()->with('success', 'Invitation envoyée !');
-    }
+   }
 }
