19ca7f377bf1bc35e79a296c581edc1d62047f0a
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-02-26 11:03:51 +0000

    feat: create InvitationController with codeGenerator and sendInvitation methods

---
+++ web/app/Http/Controllers/InvitationController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class InvitationController extends Controller
+{
+    public function codeGenerator()
+    {
+        
+    }
+    public function sendInvitation(Request $request)
+    {
+        
+    }
+}
