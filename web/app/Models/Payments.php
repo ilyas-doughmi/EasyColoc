938675c997d1282583db094fb3ece05f00a2ff62
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-02-27 14:12:49 +0000

    feat: define fillable attributes and relationships in Payments model

--- web/app/Models/Payments.php
+++ web/app/Models/Payments.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payments extends Model
 {
-    //
+    protected $fillable = [
+        'expense_id',
+        'sender_id',
+        'receiver_id',
+        'amount',
+        'status'
+    ];
+
+    public function expense()
+    {
+        return $this->belongsTo(Expense::class);
+    }
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
 }
