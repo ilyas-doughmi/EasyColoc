3b1b41cd2a8f11712a833ea0d2235bebb80e6d04
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-02-27 14:12:53 +0000

    feat: implement Expense model with fillable attributes and relationships

--- web/app/Models/Expense.php
+++ web/app/Models/Expense.php
@@ -6,5 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Expense extends Model
 {
-    //
+    protected $fillable = [
+        'colocation_id',
+        'user_id',
+        'category_id',
+        'title',
+        'amount',
+        'date'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(Payments::class);
+    }
 }
