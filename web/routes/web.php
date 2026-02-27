34886563ed75d2b0fe9e20cddb364124a11fc778
Ilyas Doughmi <doughmi.projects@gmail.com>
2026-02-27 10:59:55 +0000

    feat: add leave and cancel routes for colocations

--- web/routes/web.php
+++ web/routes/web.php
@@ -23,6 +23,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/colocations', [ColocationController::class, 'index'])->name('colocations.index');
     Route::get('/colocations/create', [ColocationController::class, 'create'])->name('colocations.create');
     Route::get('/colocations/{colocation}', [ColocationController::class, 'show'])->name('colocations.show');
+    Route::post('/colocations/{colocation}/leave', [ColocationController::class, 'leave'])->name('colocations.leave');
+    Route::post('/colocations/{colocation}/cancel', [ColocationController::class, 'cancel'])->name('colocations.cancel');
     Route::post('/colocations', [ColocationController::class, 'store'])->name('colocations.store');
     Route::post('/invitations', [InvitationController::class, 'sendInvitation'])->name('invitations.send');
     Route::post('/invitations/join', [InvitationController::class, 'join'])->name('invitations.join');
